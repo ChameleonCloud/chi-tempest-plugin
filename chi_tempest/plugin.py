baca73ffabb8054398bc3ba41007ad16cf9598b0
Michael Sherman <shermanm@uchicago.edu>
2024-07-14 20:46:36 +0000

    fix paths again

---
+++ chi_tempest/plugin.py
@@ -0,0 +1,19 @@
+import os
+
+from tempest import config
+from tempest.test_discover import plugins
+
+
+class ChiTests(plugins.TempestPlugin):
+    def load_tests(self):
+        base_path = os.path.split(os.path.dirname(
+            os.path.abspath(__file__)))[0]
+        test_dir = "chi_tempest/tests"
+        full_test_dir = os.path.join(base_path, test_dir)
+        return full_test_dir, base_path
+    
+    def register_opts(self, conf):
+        pass
+    
+    def get_opt_lists(self):
+        pass
